ec260cfaf7d5cd8d3fa756e9adaa3e343edce09b
konomae <konomae@users.noreply.github.com>
2015-01-19 02:23:06 +0900

    Cleanup order

--- tests/run.sh
+++ tests/run.sh
@@ -7,13 +7,13 @@ EXTRA_VARS="mackerel_agent_apikey=$MACKEREL_APIKEY"
 # Tests
 ansible-playbook $PLAYBOOK --syntax-check
 ansible-playbook -c local -e $EXTRA_VARS $PLAYBOOK
-ansible-playbook -c local -e $EXTRA_VARS $PLAYBOOK | \
-	grep -q 'changed=0\s*unreachable=0\s*failed=0' && exit 0 || exit 1
-
 
 # Cleanup
-service mackerel-agent stop
 HOSTID=$(cat /var/lib/mackerel-agent/id)
 curl -H "X-Api-Key: $MACKEREL_APIKEY" \
 	 -H "Content-Type: application/json" \
-     -X POST -d "{}" "https://mackerel.io/api/v0/hosts/$HOSTID/retire"
+	 -X POST -d "{}" "https://mackerel.io/api/v0/hosts/$HOSTID/retire"
+
+# Tests
+ansible-playbook -c local -e $EXTRA_VARS $PLAYBOOK | \
+	grep -q 'changed=0\s*unreachable=0\s*failed=0' && exit 0 || exit 1
